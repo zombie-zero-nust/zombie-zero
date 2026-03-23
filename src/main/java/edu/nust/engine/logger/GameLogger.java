65d24b7cdc84e1f6b43c858d5d26c363829800c5
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2026-03-23 15:59:51 +0500

    Update caller info, to use Logback instead of manually determining number

--- src/main/java/edu/nust/engine/logger/GameLogger.java
+++ src/main/java/edu/nust/engine/logger/GameLogger.java
@@ -2,17 +2,26 @@ package edu.nust.engine.logger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
 
 public class GameLogger
 {
-    private final Logger rawLogger;
-    private final String className;
+    private final LocationAwareLogger rawLogger;
+    private static final String FQCN = GameLogger.class.getName();
+    private static final String PROGRESS_FQCN = LogProgress.class.getName();
 
     /// Use {@link #getLogger(Class)} instead
     private GameLogger(Class<?> loggingClass)
     {
-        this.rawLogger = LoggerFactory.getLogger(loggingClass);
-        this.className = loggingClass.getName();
+        Logger logger = LoggerFactory.getLogger(loggingClass);
+        if (logger instanceof LocationAwareLogger)
+        {
+            this.rawLogger = (LocationAwareLogger) logger;
+        }
+        else
+        {
+            throw new UnsupportedOperationException("Logger does not support location awareness.");
+        }
     }
 
     public static GameLogger getLogger(Class<?> loggingClass) { return new GameLogger(loggingClass); }
@@ -21,31 +30,10 @@ public class GameLogger
 
     private String resetAnsi() { return "\u001B[0m"; }
 
-    /// Returns caller info in format "FileName:LineNumber". If the caller info cannot be determined, returns
-    /// "Unknown".
-    /// <br>
-    /// Does not include the `.java` extension in the file name for brevity.
-    private String getCallerInfo()
-    {
-        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
-        // loop until we find stack frame of logging class
-        for (StackTraceElement frame : stack)
-        {
-            // get simple name from fully-qualified class
-            String fullName = frame.getClassName(); // e.g., edu.nust.engine.TestLogger
-            if (fullName.equalsIgnoreCase(className))
-            {
-                String fileName = frame.getFileName();
-                if (fileName != null) return fileName.replace(".java", "") + ":" + frame.getLineNumber();
-            }
-        }
-        return "Unknown";
-    }
-
     private String getPrefix(String level, String ansiColor)
     {
-        String levelStr = "[" + level + " ";
-        return ansiColor + levelStr + getCallerInfo() + "]" + resetAnsi() + " ";
+        String levelStr = "[" + level + "]";
+        return ansiColor + levelStr + resetAnsi() + " ";
     }
 
     private String withMessage(LogLevel level, String... message)
@@ -63,48 +51,58 @@ public class GameLogger
 
     private void logMessage(LogLevel level, String message, Object... args)
     {
-        rawLogger.info(withMessage(level, message), args);
-    }
-
-    private void logProgressMessage(LogProgressType type, LogProgress progress, String message, Object... args)
-    {
-        rawLogger.info(withProgressMessage(type, progress, message), args);
+        rawLogger.log(null, FQCN, LocationAwareLogger.INFO_INT, withMessage(level, message), args, null);
     }
 
     /* LOG TYPES */
 
     /// Debug log with magenta text
-    public void debug(String message, Object... args) { logMessage(LogLevel.DEBUG, message, args); }
+    public void debug(String message, Object... args)
+    {
+        rawLogger.log(null, FQCN, LocationAwareLogger.INFO_INT, withMessage(LogLevel.DEBUG, message), args, null);
+    }
 
     /// Info log with blue text
-    public void info(String message, Object... args) { logMessage(LogLevel.INFO, message, args); }
+    public void info(String message, Object... args)
+    {
+        rawLogger.log(null, FQCN, LocationAwareLogger.INFO_INT, withMessage(LogLevel.INFO, message), args, null);
+    }
 
     /// Success log with green text
-    public void success(String message, Object... args) { logMessage(LogLevel.SUCCESS, message, args); }
+    public void success(String message, Object... args)
+    {
+        rawLogger.log(null, FQCN, LocationAwareLogger.INFO_INT, withMessage(LogLevel.SUCCESS, message), args, null);
+    }
 
     /// Warning log with yellow text
-    public void warn(String message, Object... args) { logMessage(LogLevel.WARN, message, args); }
+    public void warn(String message, Object... args)
+    {
+        rawLogger.log(null, FQCN, LocationAwareLogger.INFO_INT, withMessage(LogLevel.WARN, message), args, null);
+    }
 
     /// Error log with red text
-    public void error(String message, Object... args) { logMessage(LogLevel.ERROR, message, args); }
+    public void error(String message, Object... args)
+    {
+        rawLogger.log(null, FQCN, LocationAwareLogger.INFO_INT, withMessage(LogLevel.ERROR, message), args, null);
+    }
 
     /* PROGRESS */
 
     /// **`INTERNAL`** Use {@link LogProgress#begin(String, Object...)} instead
     void beginProgress(LogProgress progress, String message, Object... args)
     {
-        logProgressMessage(LogProgressType.BEGIN, progress, message, args);
+        rawLogger.log(null, PROGRESS_FQCN, LocationAwareLogger.INFO_INT, withProgressMessage(LogProgressType.BEGIN, progress, message), args, null);
     }
 
     /// **`INTERNAL`** Use {@link LogProgress#log(String, Object...)} instead
     void logProgress(LogProgress progress, String message, Object... args)
     {
-        logProgressMessage(LogProgressType.LOG, progress, message, args);
+        rawLogger.log(null, PROGRESS_FQCN, LocationAwareLogger.INFO_INT, withProgressMessage(LogProgressType.LOG, progress, message), args, null);
     }
 
     /// **`INTERNAL`** Use {@link LogProgress#end(String, Object...)} instead
     void endProgress(LogProgress progress, String message, Object... args)
     {
-        logProgressMessage(LogProgressType.END, progress, message, args);
+        rawLogger.log(null, PROGRESS_FQCN, LocationAwareLogger.INFO_INT, withProgressMessage(LogProgressType.END, progress, message), args, null);
     }
 }
