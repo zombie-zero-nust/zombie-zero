90d6adb53a548a1ab1adb53caccbadea5baa3462
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-04-10 13:27:02 +0500

    Health Bar Logic was changed

--- src/main/java/edu/nust/game/gameobjects/HealthBar.java
+++ src/main/java/edu/nust/game/gameobjects/HealthBar.java
@@ -43,6 +43,43 @@ public class HealthBar extends Bar
 
         updateBar(healthSystem.getCurrentHealth(), MAX_HEALTH);
     }
+
+    /**
+     * Gets the health state for sprite-based UI display
+     * 0-33% = EMPTY, 34-66% = HALF, 67-100% = FULL
+     */
+    public HealthState getHealthState(int currentHealth)
+    {
+        int percentage = (currentHealth * 100) / MAX_HEALTH;
+        if (percentage <= 33)
+            return HealthState.EMPTY;
+        else if (percentage <= 66)
+            return HealthState.HALF;
+        else
+            return HealthState.FULL;
+    }
+
+    /**
+     * Enum representing health states for sprite display
+     */
+    public enum HealthState
+    {
+        FULL("Hunger_Full.png"),
+        HALF("Hunger_Half.png"),
+        EMPTY("Hunger_Empty.png");
+
+        private final String spriteFile;
+
+        HealthState(String spriteFile)
+        {
+            this.spriteFile = spriteFile;
+        }
+
+        public String getSpriteFile()
+        {
+            return spriteFile;
+        }
+    }
 }
 
 
