5ea98bca7ffdc307e7971587a207516fe6235369
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2026-04-20 17:41:23 +0500

    Add Build Configs for Windows, Linux and Mac along with executable jar

---
+++ src/main/java/edu/nust/Launcher.java
@@ -0,0 +1,11 @@
+package edu.nust;
+
+/**
+ * Plain Java entrypoint used by installDist/jpackage launchers.
+ */
+public final class Launcher
+{
+    private Launcher() { }
+    public static void main(String[] args) { Main.main(args); }
+}
+
