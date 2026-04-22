e37ec736a2da2070d282a6a5da4b8593b8bc1066
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2026-04-22 17:51:20 +0500

    Complete Collision Map For Level 1

--- src/game/java/edu/nust/game/scenes/levelscene/level_1/Level1CollisionMask.java
+++ src/game/java/edu/nust/game/scenes/levelscene/level_1/Level1CollisionMask.java
@@ -14,10 +14,14 @@ public final class Level1CollisionMask
             Rectangle.fromCorners(3196, 4, 3250, 794), // Right
             Rectangle.fromCorners(-50, 794, 3250, 850), // Bottom
             /// Ground
-            Rectangle.fromCorners(4, 108, 68, 661), // Ground Left-Middle
-            Rectangle.fromCorners(108, 4, 852, 308), // Ground Top Left 1
-            Rectangle.fromCorners(892, 4, 1444, 308), // Ground Top Left 2
-            Rectangle.fromCorners(1484, 76, 1596, 724), // Ground Right-Middle
+            // Ground Left-Middle
+            Rectangle.fromCorners(4, 108, 68, 661),
+            // Ground Top Left 1
+            Rectangle.fromCorners(108, 4, 852, 308),
+            // Ground Top Left 2
+            Rectangle.fromCorners(892, 4, 1444, 308),
+            // Ground Right-Middle
+            Rectangle.fromCorners(1484, 76, 1600, 724),
             // Ground Bottom Left 1
             Rectangle.fromCorners(108, 364, 204, 660), // left
             Rectangle.fromCorners(204, 364, 612, 794), // right (rest)
@@ -28,6 +32,38 @@ public final class Level1CollisionMask
             Rectangle.fromCorners(1060, 364, 1444, 564), // above path
             Rectangle.fromCorners(1060, 604, 1444, 794), // below path
             /// City
+            // Building 1
+            Rectangle.fromCorners(1648, 48, 1664, 256), // outer left
+            Rectangle.fromCorners(1664, 48, 2000, 64), // outer top
+            Rectangle.fromCorners(1986, 64, 2000, 256), // outer right
+            Rectangle.fromCorners(1836, 240, 1986, 256), // outer bottom right
+            Rectangle.fromCorners(1664, 240, 1748, 256), // outer bottom left
+            Rectangle.fromCorners(1776, 64, 1792, 144), // inner top 1
+            Rectangle.fromCorners(1904, 64, 1920, 164), // inner top 2.1
+            Rectangle.fromCorners(1920, 96, 1972, 112), // inner top 2.2
+            Rectangle.fromCorners(1712, 140, 1728, 240), // inner bottom 1.1
+            Rectangle.fromCorners(1728, 176, 1844, 192), // inner bottom 1.2
+            // Ground
+            Rectangle.fromCorners(1648, 304, 2000, 752),
+            // Building 2
+            Rectangle.fromCorners(2048, 304, 2064, 340), // outer left top
+            Rectangle.fromCorners(2064, 304, 2384, 320), // outer top
+            Rectangle.fromCorners(2368, 320, 2384, 496), // outer right
+            Rectangle.fromCorners(2048, 480, 2368, 496), // outer bottom
+            Rectangle.fromCorners(2048, 428, 2064, 480), // outer left bottom
+            Rectangle.fromCorners(2256, 396, 2272, 480), // inner bottom 1.1
+            Rectangle.fromCorners(2256, 348, 2272, 370), // inner bottom 1.2
+            Rectangle.fromCorners(2076, 400, 2160, 416), // inner left 1.1
+            Rectangle.fromCorners(2128, 416, 2144, 448), // inner left 1.2
+            // Building 3
+            Rectangle.fromCorners(2124, 544, 2384, 560), // outer top
+            Rectangle.fromCorners(2368, 560, 2384, 752), // outer right
+            Rectangle.fromCorners(2048, 736, 2368, 752), // outer bottom
+            Rectangle.fromCorners(2048, 652, 2064, 736), // outer left
+            Rectangle.fromCorners(2208, 652, 2224, 736), // inner bottom
+            Rectangle.fromCorners(2320, 602, 2336, 644), // inner top 1
+            Rectangle.fromCorners(2128, 588, 2144, 656), // inner top 2.1
+            Rectangle.fromCorners(2096, 656, 2200, 672), // inner top 2.2
     };
 
     public boolean isWalkable(Vector2D pos)
